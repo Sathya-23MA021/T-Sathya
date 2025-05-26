95a7dcb3c955cd7c32fdb8045a55218ed908ea25
sathya <sarathisathya11@gmail.com>
2025-05-26 16:10:41 +0530

    day11

--- Desktop/day7/sarathi/models.py
+++ Desktop/day7/sarathi/models.py
@@ -17,4 +17,9 @@ class biodata(models.Model):
     gender=models.CharField(max_length=50)
     mobile_no = models.CharField(max_length=15)  # Adjust max_length as needed
     stream=models.CharField(max_length=50)
+ 
+
+    def __str__(self):
+        return self.mobile_no
+
 
--- Desktop/day7/sarathi/views.py
+++ Desktop/day7/sarathi/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import get_object_or_404, render, redirect
 from .models import *
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
@@ -75,4 +75,28 @@ def student_biodata(request):
     biodatas = biodata.objects.all()
     return render(request,'biodata.html', {'biodatas': biodatas})    
 
-    
+
+
+
+def edit_biodata(request, pk):
+    biodatas = get_object_or_404(biodata, pk=pk)
+
+    if request.method == "POST":
+        biodatas.dept = request.POST.get("dept")
+        biodatas.age = request.POST.get("age")
+        biodatas.gender  = request.POST.get("gender")
+        biodatas.mobile_no = request.POST.get("mobile_no")
+        biodatas.stream = request.POST.get("stream")
+        biodatas.save()
+        return redirect("edit_biodata")
+
+    return render(request, "edit.html", {
+        "edit_biodata": biodatas,
+        "biodatas": biodata.objects.all()
+    })
+
+
+def delete_biodata(request, pk):
+    biodatas = get_object_or_404(biodata, pk=pk)
+    biodatas.delete()
+    return redirect("delete_biodata")
